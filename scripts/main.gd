994d6202c219c132856b7bce2dd53de33b4e46dd
skelseed <crazypotato73@aol.com>
2026-06-30 18:49:36 -0400

    fix right click beta

--- scripts/main.gd
+++ scripts/main.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-var move_speed = 3
+var move_speed = 1
 var direction = Vector2(1,0) #moving right
 
 # Called when the node enters the scene tree for the first time.
@@ -29,11 +29,13 @@ func _process(_delta):
 	# screen.get_usable_rect() returns screen area MINUS taskbar or docs linux
 	var usable_rect = DisplayServer.screen_get_usable_rect()
 	
+	var bounding = true
 	#if right side of window > right side of screen
-	if window.position.x + window.size.x > usable_rect.end.x:
-		direction.x = -1
-		$AnimatedSprite2D.flip_h = false
+	if bounding == true:
+		if window.position.x + window.size.x > usable_rect.end.x:
+			direction.x = -1
+			$AnimatedSprite2D.flip_h = false
 
-	elif window.position.x < usable_rect.position.x:
-		direction.x = 1
-		$AnimatedSprite2D.flip_h = true
+		elif window.position.x < usable_rect.position.x:
+			direction.x = 1
+			$AnimatedSprite2D.flip_h = true
