a5c416a46176d125ba6f322a2b8956c715ae8251
skelseed <crazypotato73@aol.com>
2026-06-30 13:38:39 -0400

    Funcionalidad no mockup

--- scripts/main.gd
+++ scripts/main.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+var move_speed = 1
+var direction = Vector2(1,0) #moving right
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -15,19 +17,6 @@ func _ready() -> void:
 	window.unresizable = false
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-var drag_offset: Vector2 = Vector2.ZERO
-var is_dragging: bool = false
-
 
 func _process(delta: float) -> void:
-	if Input.is_action_just_pressed("click"): ###LMB
-		is_dragging = true
-		drag_offset = get_viewport().get_mouse_position()
-	if Input.is_action_just_released("click"): ###As I said
-		is_dragging = false
-		drag_offset = Vector2.ZERO
-	if is_dragging:
-		var current_mouse_pos: Vector2 = DisplayServer.mouse_get_position()
-		var window_pos: Vector2 = current_mouse_pos - drag_offset
-		DisplayServer.window_set_position(window_pos)
+	pass
